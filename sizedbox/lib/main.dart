af1d9da569f23bdf88f483b7d1d494ff7db3fb41
vudv <vudvhitech@gmail.com>
2019-04-16 13:03:34 +0700

    first commit

---
+++ sizedbox/lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SizedBox',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'SizedBox Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  bool isVisible = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: sizedBoxVisible(),
+    );
+  }
+
+  Widget sizedBoxExpand(){
+    return SizedBox.expand(
+      child: Card(
+          child: Center(child: Text('Hello World!',style: TextStyle(color: Colors.white),),),
+          color: Colors.green
+      ),
+    );
+  }
+
+  Widget sizedBoxPadding(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Column(
+          children: <Widget>[
+            Icon(Icons.star, size: 50,color: Colors.green,),
+            const SizedBox(height: 100.0),
+            Icon(Icons.star, size: 50,color: Colors.green),
+            Icon(Icons.star, size: 50,color: Colors.green),
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget sizedBoxVisible(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Column(
+          children: <Widget>[
+            isVisible
+                ? Icon(Icons.star, size: 150,color: Colors.green,)
+                : const SizedBox(),
+            FlatButton(
+              child: Text("Demo"),
+              onPressed: () {
+                setState(() {
+                  isVisible = !isVisible;
+                });
+              },
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
