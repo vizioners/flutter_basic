bf176c9dd02eba2e7f6af1535d73e0f91747036a
vudv <vudvhitech@gmail.com>
2019-04-19 15:00:45 +0700

    ConstainedBox

---
+++ constrained_box/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'ConstrainedBox'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ConstrainedBox(
+        constraints: BoxConstraints.expand(height: 300.0,width: 200.0),
+        child: const Card(
+          child: const Text('Hello World!'),
+          color: Colors.yellow,
+        ),
+      ),
+    );
+  }
+}
